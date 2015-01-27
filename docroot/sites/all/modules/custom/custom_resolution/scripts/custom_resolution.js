baea3c1158fdc2750b440fd2e705298d3173f48c
Ignat Volosky <svetozar8720@gmail.com>
2015-01-27 15:07:54 +0200

    Hide checked delegates on resolion form in another tabs

--- docroot/sites/all/modules/custom/custom_resolution/scripts/custom_resolution.js
+++ docroot/sites/all/modules/custom/custom_resolution/scripts/custom_resolution.js
@@ -1,12 +1,15 @@
 (function ($) {
   var selectedParty;
-
+  var firstCall = true;
   $(document).ready(function() {
     var fractions = $('.views-field-field-fraction');
     var partiesFilter = $('#edit-supported-parties-filter');
+    var partyDelegateCheckbox = $('.group-resolution-votes').find('.form-checkbox');
     selectedParty = partiesFilter.val();
     fractions.each(toggleRows);
     partiesFilter.change(filterDelegates);
+    partyDelegateCheckbox.each(hideCheckedDelegates);
+    partyDelegateCheckbox.click(hideCheckedDelegates);
   });
 
   function filterDelegates() {
@@ -17,13 +20,38 @@
 
   function toggleRows() {
     var currentFraction = $(this).children('.field-content').text();
-    var formItem = $(this).parents('.form-type-checkbox');
+    var formItem = $(this).parents('.form-type-checkbox').not('.already-checked');
 
     if (currentFraction !== selectedParty) {
       formItem.hide();
     } else {
       formItem.show();
-      console.log(true);
+    }
+  }
+
+  function hideCheckedDelegates() {
+    var firstCall = false;
+    var anotherTabs = $(this).parents('.field-group-tab').siblings();
+    var checkedDelegate = $(this).next().text();
+    var anotherTabsCheckboxes = anotherTabs.find('.form-checkbox');
+    if ($(this).is(':checked')) {
+      anotherTabsCheckboxes.each(function() {
+        var checkBoxText = $(this).next().text();
+        if (checkBoxText === checkedDelegate) {
+          var formItem = $(this).parent();
+          formItem.hide();
+          formItem.addClass('already-checked');
+        }
+      });
+    } else {
+      anotherTabsCheckboxes.each(function() {
+        var checkBoxText = $(this).next().text();
+        if (checkBoxText === checkedDelegate && firstCall) {
+          var formItem = $(this).parent();
+          formItem.hide();
+          formItem.removeClass('already-checked');
+        }
+      });
     }
   }
 
