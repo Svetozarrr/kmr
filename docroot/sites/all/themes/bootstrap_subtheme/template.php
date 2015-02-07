104aea905115407f7fdcb998648bfc07a1929127
Ignat Volosky <svetozar8720@gmail.com>
2015-02-07 23:42:31 +0200

    Add debug info for html array

--- docroot/sites/all/themes/bootstrap_subtheme/template.php
+++ docroot/sites/all/themes/bootstrap_subtheme/template.php
@@ -14,4 +14,5 @@ function bootstrap_subtheme_preprocess_html(&$variables)
   $head_title = &$variables['head_title'];
   $head_title = htmlspecialchars_decode($head_title);
   $head_title = strip_tags($head_title);
+  dpm($head_title);
 }
