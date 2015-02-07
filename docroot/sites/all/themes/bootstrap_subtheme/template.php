88e2fa4e9218f27b0b788f3ae66c8d3817e715cb
Ignat Volosky <svetozar8720@gmail.com>
2015-02-07 23:32:36 +0200

    Add debug info for html array

--- docroot/sites/all/themes/bootstrap_subtheme/template.php
+++ docroot/sites/all/themes/bootstrap_subtheme/template.php
@@ -2,5 +2,13 @@
 
 /**
  * @file
- * template.php
+ * Contains custom preprocess functions for current theme
  */
+
+/**
+ * Implements template_preprocess_html()
+ */
+function bootstrap_subtheme_preprocess_html(&$variables)
+{
+  dpm($variables);
+}
