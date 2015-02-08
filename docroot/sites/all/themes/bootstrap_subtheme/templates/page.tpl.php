4910310bd68b0802822d316d606d7a5ee9785c0a
Ignat Volosky <svetozar8720@gmail.com>
2015-02-08 02:17:43 +0200

    Change site logo styles

--- docroot/sites/all/themes/bootstrap_subtheme/templates/page.tpl.php
+++ docroot/sites/all/themes/bootstrap_subtheme/templates/page.tpl.php
@@ -79,7 +79,7 @@
       <?php if ($logo): ?>
       <div class="logo-container col-sm-3">
         <a class="logo" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          <img class="logo-image pull-left" src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
           <?php if (!empty($site_slogan)): ?>
             <h2 class="site-slogan"><?php print $site_slogan; ?></h2>
           <?php endif; ?>
